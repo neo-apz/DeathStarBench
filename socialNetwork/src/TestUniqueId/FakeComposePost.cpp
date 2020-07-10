34d9a9c839450dea0f3c6b93ba05d86c4bf42ee6
Arash Pourhabibi <arash.pourhabibi@epfl.ch>
2020-07-10 17:44:01 +0200

    Add test for UniqueIdService.

---
+++ socialNetwork/src/TestUniqueId/FakeComposePost.cpp
@@ -0,0 +1,84 @@
+#include <thrift/protocol/TCompactProtocol.h>
+#include <thrift/server/TThreadedServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <signal.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+
+#include "../../gen-cpp/ComposePostService.h"
+
+
+using apache::thrift::server::TThreadedServer;
+using apache::thrift::transport::TServerSocket;
+using apache::thrift::transport::TFramedTransportFactory;
+using apache::thrift::protocol::TCompactProtocolFactory;
+using namespace social_network;
+
+
+namespace social_network {
+
+class ComposePostHandler : public ComposePostServiceIf {
+ public:
+  ComposePostHandler();
+  ~ComposePostHandler() override = default;
+
+  void UploadText(int64_t req_id, const std::string& text) override;
+
+  void UploadMedia(int64_t req_id, const std::vector<Media>& media) override;
+
+  void UploadUniqueId(int64_t req_id, const int64_t post_id,
+      const PostType::type post_type) override;
+
+  void UploadCreator(int64_t req_id, const Creator& creator) override;
+
+  void UploadUrls(int64_t req_id, const std::vector<Url> & urls) override;
+
+  void UploadUserMentions(const int64_t req_id,
+      const std::vector<UserMention> & user_mentions) override;
+
+};
+
+ComposePostHandler::ComposePostHandler() {}
+
+void ComposePostHandler::UploadCreator( int64_t req_id, const Creator &creator) {}
+
+void ComposePostHandler::UploadText(int64_t req_id, const std::string &text) { }
+
+void ComposePostHandler::UploadMedia(int64_t req_id, const std::vector<Media> &media) {}
+
+void ComposePostHandler::UploadUniqueId(int64_t req_id, const int64_t post_id, const PostType::type post_type) {}
+
+void ComposePostHandler::UploadUrls(int64_t req_id, const std::vector<Url> &urls) {}
+
+void ComposePostHandler::UploadUserMentions(const int64_t req_id, const std::vector<UserMention> &user_mentions) {}
+
+
+void sigintHandler(int sig) {
+  exit(EXIT_SUCCESS);
+}
+
+}// namespace social_network
+
+int main(int argc, char *argv[]) {
+  signal(SIGINT, sigintHandler);
+
+  int port = 5000;
+  
+  TThreadedServer server(
+      std::make_shared<ComposePostServiceProcessor>(
+          std::make_shared<ComposePostHandler>()),
+      std::make_shared<TServerSocket>("0.0.0.0", port),
+      std::make_shared<TFramedTransportFactory>(),
+      std::make_shared<TCompactProtocolFactory>()
+  );
+  
+  std::cout << "Starting the compose-post-service server ..." << std::endl;
+  server.serve();
+
+  return 0;
+}
---
+++ socialNetwork/src/TestUniqueId/RequestGen.cpp
@@ -0,0 +1,70 @@
+#include <signal.h>
+#include <string>
+#include <thread>
+#include <iostream>
+
+#include <thrift/server/TThreadedServer.h>
+#include <thrift/protocol/TCompactProtocol.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+
+#include "../../gen-cpp/UniqueIdService.h"
+#include "../../gen-cpp/social_network_types.h"
+
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+#include <thrift/stdcxx.h>
+
+
+using apache::thrift::transport::TFramedTransportFactory;
+using apache::thrift::protocol::TCompactProtocolFactory;
+using apache::thrift::protocol::TCompactProtocol;
+using apache::thrift::protocol::TProtocol;
+using apache::thrift::protocol::TBinaryProtocol;
+using apache::thrift::transport::TFramedTransport;
+using apache::thrift::transport::TSocket;
+using apache::thrift::transport::TTransport;
+
+using namespace social_network;
+
+void sigintHandler(int sig) {
+  exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+  signal(SIGINT, sigintHandler);
+  // init_logger();
+  // SetUpTracer("config/jaeger-config.yml", "unique-id-service");
+
+  // json config_json;
+  // if (load_config_file("config/service-config.json", &config_json) != 0) {
+  //   exit(EXIT_FAILURE);
+  // }
+
+  int server_port = 4000;
+
+  std::string server_addr = "127.0.0.1";
+
+  std::shared_ptr<TTransport> socket = std::shared_ptr<TTransport>(new TSocket(server_addr, server_port));;
+  std::shared_ptr<TTransport> transport = std::shared_ptr<TTransport>(new TFramedTransport(socket));
+  std::shared_ptr<TProtocol> protocol = std::shared_ptr<TProtocol>(new TCompactProtocol(transport));
+
+  UniqueIdServiceClient *client = new UniqueIdServiceClient(protocol);
+
+  transport->open();
+
+  std::cout << "Starting generating requests ..." << std::endl;
+
+  int64_t req_id = 0xFFFFFFFFFFFF; // rand!
+  PostType::type post_type = (PostType::type) 0;
+
+
+  while (true)
+  {
+      client->UploadUniqueId(req_id, post_type);
+  }
+
+  transport->close();
+  
+  
+}
