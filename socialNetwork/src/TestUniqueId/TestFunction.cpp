35ca5cbc9ca6bc49735af63a3087744a10209bd9
Arash Pourhabibi <arash.pourhabibi@epfl.ch>
2020-07-10 22:01:54 +0200

    Add test function.

---
+++ socialNetwork/src/TestUniqueId/TestFunction.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <mutex>
+#include <sstream>
+#include <iomanip>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+
+#include "../../gen-cpp/social_network_types.h"
+
+// Custom Epoch (January 1, 2018 Midnight GMT = 2018-01-01T00:00:00Z)
+#define CUSTOM_EPOCH 1514764800000
+
+namespace social_network {
+
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
+using std::chrono::system_clock;
+
+static int64_t current_timestamp = -1;
+static int counter = 0;
+
+static int GetCounter(int64_t timestamp) {
+  if (current_timestamp > timestamp) {
+    exit(EXIT_FAILURE);
+  }
+  if (current_timestamp == timestamp) {
+    return counter++;
+  } else {
+    current_timestamp = timestamp;
+    counter = 0;
+    return counter++;
+  }
+}
+
+class UniqueIdHandler {
+ public:
+  ~UniqueIdHandler() = default;
+  UniqueIdHandler(std::mutex *, const std::string &);
+
+  int64_t UploadUniqueId(int64_t, PostType::type);
+
+ private:
+  std::mutex *_thread_lock;
+  std::string _machine_id;
+};
+
+UniqueIdHandler::UniqueIdHandler(std::mutex *thread_lock, const std::string &machine_id) {
+  _thread_lock = thread_lock;
+  _machine_id = machine_id;
+}
+
+ int64_t UniqueIdHandler::UploadUniqueId(int64_t req_id, PostType::type post_type) {
+
+  _thread_lock->lock();
+  int64_t timestamp = duration_cast<milliseconds>(
+      system_clock::now().time_since_epoch()).count() - CUSTOM_EPOCH;
+  int idx = GetCounter(timestamp);
+  _thread_lock->unlock();
+
+  std::stringstream sstream;
+  sstream << std::hex << timestamp;
+  std::string timestamp_hex(sstream.str());
+
+  if (timestamp_hex.size() > 10) {
+    timestamp_hex.erase(0, timestamp_hex.size() - 10);
+  } else if (timestamp_hex.size() < 10) {
+    timestamp_hex = std::string(10 - timestamp_hex.size(), '0') + timestamp_hex;
+  }
+
+  // Empty the sstream buffer.
+  sstream.clear();
+  sstream.str(std::string());
+
+  sstream << std::hex << idx;
+  std::string counter_hex(sstream.str());
+
+  if (counter_hex.size() > 3) {
+    counter_hex.erase(0, counter_hex.size() - 3);
+  } else if (counter_hex.size() < 3) {
+    counter_hex = std::string(3 - counter_hex.size(), '0') + counter_hex;
+  }
+
+  std::string post_id_str = _machine_id + timestamp_hex + counter_hex;
+  int64_t post_id = stoul(post_id_str, nullptr, 16) & 0x7FFFFFFFFFFFFFFF;
+
+  return post_id;
+}
+
+/*
+ * The following code which obtaines machine ID from machine's MAC address was
+ * inspired from https://stackoverflow.com/a/16859693.
+ */
+u_int16_t HashMacAddressPid(const std::string &mac)
+{
+  u_int16_t hash = 0;
+  std::string mac_pid = mac + std::to_string(getpid());
+  for ( unsigned int i = 0; i < mac_pid.size(); i++ ) {
+    hash += ( mac[i] << (( i & 1 ) * 8 ));
+  }
+  return hash;
+}
+
+int GetMachineId (std::string *mac_hash) {
+  std::string mac;
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP );
+  if ( sock < 0 ) {
+    return -1;
+  }
+
+  struct ifconf conf{};
+  char ifconfbuf[ 128 * sizeof(struct ifreq)  ];
+  memset( ifconfbuf, 0, sizeof( ifconfbuf ));
+  conf.ifc_buf = ifconfbuf;
+  conf.ifc_len = sizeof( ifconfbuf );
+  if ( ioctl( sock, SIOCGIFCONF, &conf ))
+  {
+    return -1;
+  }
+
+  struct ifreq* ifr;
+  for (
+      ifr = conf.ifc_req;
+      reinterpret_cast<char *>(ifr) <
+          reinterpret_cast<char *>(conf.ifc_req) + conf.ifc_len;
+      ifr++) {
+    if ( ifr->ifr_addr.sa_data == (ifr+1)->ifr_addr.sa_data ) {
+      continue;  // duplicate, skip it
+    }
+
+    if ( ioctl( sock, SIOCGIFFLAGS, ifr )) {
+      continue;  // failed to get flags, skip it
+    }
+    if ( ioctl( sock, SIOCGIFHWADDR, ifr ) == 0 ) {
+      mac = std::string(ifr->ifr_addr.sa_data);
+      if (!mac.empty()) {
+        break;
+      }
+    }
+  }
+  close(sock);
+
+  std::stringstream stream;
+  stream << std::hex << HashMacAddressPid(mac);
+  *mac_hash = stream.str();
+
+  if (mac_hash->size() > 3) {
+    mac_hash->erase(0, mac_hash->size() - 3);
+  } else if (mac_hash->size() < 3) {
+    *mac_hash = std::string(3 - mac_hash->size(), '0') + *mac_hash;
+  }
+  return 0;
+}
+
+} // namespace social_network
+
+using namespace social_network;
+
+int main(int argc, char *argv[]) {
+
+    std::mutex thread_lock;
+
+    std::string machine_id;
+    if (GetMachineId(&machine_id) != 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    cpu_set_t  mask;
+    CPU_ZERO(&mask);
+    CPU_SET(4, &mask);
+    sched_setaffinity(0, sizeof(mask), &mask);
+
+    UniqueIdHandler* handler = new UniqueIdHandler(&thread_lock, machine_id);
+
+    int64_t req_id = 0xFFFFFFFFFFFF; // rand!
+    PostType::type post_type = (PostType::type) 0;
+
+    int64_t* ids = (int64_t*) malloc(sizeof(int64_t) * 400000000);
+
+    uint64_t i = 0;
+
+    while (i < 400000000)
+    {
+        ids[i++] = handler->UploadUniqueId(req_id, post_type);
+        // std::cout << ids[i-1] << std::endl;
+    }
+    
+
+    return 0;
+}
