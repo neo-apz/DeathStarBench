576b0d019125ad4eb736eeac5ed731501b02e969
Arash Pourhabibi <arash.pourhabibi@epfl.ch>
2021-01-04 17:00:09 +0100

    Remove extra magicBP def.

--- socialNetwork/MySrc/MyCommon/MagicBreakPoint.h
+++ socialNetwork/MySrc/MyCommon/MagicBreakPoint.h
@@ -25,26 +25,26 @@
     #define MAGIC_SERVICE_END           1700
 #endif
 
-static inline __attribute__ ((always_inline))
-    uint64_t  call_magic_2_64(uint64_t cmd_id, uint64_t arg1, uint64_t arg2){
-#ifdef __aarch64__
-        uint64_t  ret_value;
-        __asm__ __volatile__ (
-                "mov x0, %1\n"
-                "mov x1, %2\n"
-                "mov x2, %3\n"
-                "orr x30,x30,x30\n" /* <------ This is where the magic happens */
-                "mov %0, x0\n"      /* Flexus can write x0 for the return value */
-                : "=r"(ret_value)     /* output registers*/
-                : "r"(cmd_id), "r"(arg1), "r"(arg2)      /* input registers*/
-                : "x0", "x1", "x2", "x30"   /* clobbered registers*/
-                );
-        return ret_value;
-#else
-        return 0;
-#endif
+// static inline __attribute__ ((always_inline))
+//     uint64_t  call_magic_2_64(uint64_t cmd_id, uint64_t arg1, uint64_t arg2){
+// #ifdef __aarch64__
+//         uint64_t  ret_value;
+//         __asm__ __volatile__ (
+//                 "mov x0, %1\n"
+//                 "mov x1, %2\n"
+//                 "mov x2, %3\n"
+//                 "orr x30,x30,x30\n" /* <------ This is where the magic happens */
+//                 "mov %0, x0\n"      /* Flexus can write x0 for the return value */
+//                 : "=r"(ret_value)     /* output registers*/
+//                 : "r"(cmd_id), "r"(arg1), "r"(arg2)      /* input registers*/
+//                 : "x0", "x1", "x2", "x30"   /* clobbered registers*/
+//                 );
+//         return ret_value;
+// #else
+//         return 0;
+// #endif
 
-    }
+//     }
 
 // static inline __attribute__ ((always_inline), (optimize("O0")))
 static inline __attribute__ ((always_inline))
