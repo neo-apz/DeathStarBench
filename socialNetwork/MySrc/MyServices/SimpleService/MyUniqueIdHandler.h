86d082cccd7832d3a20d94f70fe3b0e85109aaa2
Arash Pourhabibi <arash.pourhabibi@epfl.ch>
2021-01-24 21:34:02 +0100

    Add SimpleService for debugging purposes.
    Update son-common.

---
+++ socialNetwork/MySrc/MyServices/SimpleService/MyUniqueIdHandler.h
@@ -0,0 +1,166 @@
+#ifndef SOCIAL_NETWORK_MICROSERVICES_MYUNIQUEIDHANDLER_H
+#define SOCIAL_NETWORK_MICROSERVICES_MYUNIQUEIDHANDLER_H
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <mutex>
+#include <sstream>
+#include <iomanip>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+
+#include "../../gen-cpp/MyUniqueIdService.h"
+#include "../../gen-cpp/my_social_network_types.h"
+
+#include "../../gen-cpp/MyComposePostService.h"
+
+#include <logger.h>
+#include <NebulaClientPool.h>
+#include <MyThriftClient.h>
+
+// Custom Epoch (January 1, 2018 Midnight GMT = 2018-01-01T00:00:00Z)
+#define CUSTOM_EPOCH 1514764800000
+
+namespace my_social_network {
+
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
+using std::chrono::system_clock;
+
+static int64_t current_timestamp = -1;
+static int counter = 0;
+
+static int GetCounter(int64_t timestamp) {
+  if (current_timestamp == timestamp) {
+    return counter++;
+  } else {
+    current_timestamp = timestamp;
+    counter = 0;
+    return counter++;
+  }
+}
+
+class MyUniqueIdHandler : public MyUniqueIdServiceIf {
+ public:
+  ~MyUniqueIdHandler() override = default;
+  MyUniqueIdHandler(
+      std::mutex *,
+      const std::string &,
+			NebulaClientPool<MyComposePostServiceClient> *);
+
+  int64_t UploadUniqueId(int64_t, PostType::type) override;
+
+ private:
+  std::mutex *_thread_lock;
+  // MyLock *_thread_lock;
+  std::string _machine_id;
+	NebulaClientPool<MyComposePostServiceClient> *_compose_client_pool;
+};
+
+MyUniqueIdHandler::MyUniqueIdHandler(
+    std::mutex *thread_lock,
+    const std::string &machine_id,
+		NebulaClientPool<MyComposePostServiceClient> * compose_client_pool){
+  _thread_lock = thread_lock;
+  _machine_id = machine_id;
+	_compose_client_pool = compose_client_pool;
+}
+
+int64_t MyUniqueIdHandler::UploadUniqueId(
+    int64_t req_id,
+    PostType::type post_type) {
+
+  int64_t timestamp = CUSTOM_EPOCH + req_id;
+  int idx = GetCounter(timestamp);
+  
+  int64_t post_id = idx & 0x7FFFFFFFFFFFFFFF;
+
+	// Upload to compose post service
+	try {
+		auto compose_post_client_wrapper = _compose_client_pool->Get(0);
+		auto compose_post_client = compose_post_client_wrapper->GetClient();
+		compose_post_client->UploadUniqueId(req_id, post_id, post_type);
+		compose_post_client_wrapper->ResetBuffers(true, false);
+	} catch(const std::exception& e) {
+		LOG(error) << "Failed to upload unique-id to compose-post-service:\n"
+							 << e.what() << '\n' ;
+		exit(EXIT_FAILURE);
+	}
+
+  return post_id;
+}
+
+/*
+ * The following code which obtaines machine ID from machine's MAC address was
+ * inspired from https://stackoverflow.com/a/16859693.
+ */
+u_int16_t HashMacAddressPid(const std::string &mac)
+{
+  u_int16_t hash = 0;
+  std::string mac_pid = mac + std::to_string(getpid());
+  for ( unsigned int i = 0; i < mac_pid.size(); i++ ) {
+    hash += ( mac[i] << (( i & 1 ) * 8 ));
+  }
+  return hash;
+}
+
+int GetMachineId (std::string *mac_hash) {
+  std::string mac;
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP );
+  if ( sock < 0 ) {
+    LOG(error) << "Unable to obtain MAC address";
+    return -1;
+  }
+
+  struct ifconf conf{};
+  char ifconfbuf[ 128 * sizeof(struct ifreq)  ];
+  memset( ifconfbuf, 0, sizeof( ifconfbuf ));
+  conf.ifc_buf = ifconfbuf;
+  conf.ifc_len = sizeof( ifconfbuf );
+  if ( ioctl( sock, SIOCGIFCONF, &conf ))
+  {
+    LOG(error) << "Unable to obtain MAC address";
+    return -1;
+  }
+
+  struct ifreq* ifr;
+  for (
+      ifr = conf.ifc_req;
+      reinterpret_cast<char *>(ifr) <
+          reinterpret_cast<char *>(conf.ifc_req) + conf.ifc_len;
+      ifr++) {
+    if ( ifr->ifr_addr.sa_data == (ifr+1)->ifr_addr.sa_data ) {
+      continue;  // duplicate, skip it
+    }
+
+    if ( ioctl( sock, SIOCGIFFLAGS, ifr )) {
+      continue;  // failed to get flags, skip it
+    }
+    if ( ioctl( sock, SIOCGIFHWADDR, ifr ) == 0 ) {
+      mac = std::string(ifr->ifr_addr.sa_data);
+      if (!mac.empty()) {
+        break;
+      }
+    }
+  }
+  close(sock);
+
+  std::stringstream stream;
+  stream << std::hex << HashMacAddressPid(mac);
+  *mac_hash = stream.str();
+
+  if (mac_hash->size() > 3) {
+    mac_hash->erase(0, mac_hash->size() - 3);
+  } else if (mac_hash->size() < 3) {
+    *mac_hash = std::string(3 - mac_hash->size(), '0') + *mac_hash;
+  }
+  return 0;
+}
+
+} // namespace my_social_network
+
+#endif //SOCIAL_NETWORK_MICROSERVICES_MYUNIQUEIDHANDLER_H
---
+++ socialNetwork/MySrc/MyServices/SimpleService/SimpleService.cpp
@@ -0,0 +1,208 @@
+#include <utils.h>
+#include <NebulaThriftProcessor.h>
+#include <RandomGenerator.h>
+#include <NebulaClientPool.h>
+
+#include "MyUniqueIdHandler.h"
+
+using namespace my_social_network;
+
+using namespace std;
+
+uint64_t num_iterations;
+std::mutex thread_lock;
+// MyLock thread_lock;
+std::string machine_id;
+
+cpu_set_t *cpuSet;
+
+volatile bool start = false;
+pthread_barrier_t barrier;
+
+
+#define BASE_BUFFER_SIZE         50
+#define NUM_TEMPLATE_CLIENTS     20
+#define NUM_MSGS_PER_CLIENT      1
+
+void ClientRecvUniqueId(MyThriftClient<MyUniqueIdServiceClient> *uniqueIdClient){
+  
+  uniqueIdClient->Connect();
+  auto client = uniqueIdClient->GetClient();
+
+  client->recv_UploadUniqueId();
+}
+
+void GenRequests(MyThriftClient<MyUniqueIdServiceClient> *clientPtr,
+								 RandomGenerator *randGen){
+
+	auto client = clientPtr->GetClient();
+	for (int i = 0; i < NUM_MSGS_PER_CLIENT; i++) {
+		int64_t req_id = randGen->getInt64(0xFFFFFFFFFFFFFF);
+	  PostType::type post_type = (PostType::type) randGen->getInt64(0, 3);
+
+  	client->send_UploadUniqueId(req_id, post_type);
+	}
+
+  // uint8_t* cltIBufPtr, *cltOBufPtr;
+  // uint32_t ISz, OSz;
+  
+  // clientPtr->GetBuffer(&cltIBufPtr, &ISz, &cltOBufPtr, &OSz);
+
+  // std::cout << "ISz: " <<  ISz << " OSz: " << OSz << std::endl;  
+}
+
+void InitializeFunctionMap(FunctionClientMap<MyComposePostServiceClient> *f2cmap, RandomGenerator *randGen) {
+	MyThriftClient<MyComposePostServiceClient>** clients = new MyThriftClient<MyComposePostServiceClient>*[NUM_TEMPLATE_CLIENTS];
+
+	// Fill up the clients
+	uint64_t buffer_size = NUM_MSGS_PER_CLIENT * BASE_BUFFER_SIZE;
+	for (int i = 0; i < NUM_TEMPLATE_CLIENTS; i++) {
+		clients[i] = new MyThriftClient<MyComposePostServiceClient>(buffer_size);
+		clients[i]->GetClient()->FakeUploadUniqueId(randGen);
+	}
+
+	f2cmap->RegisterFunction(0, clients);
+}
+
+void GenAndProcessReqs(rpcNUMAContext* ctx,
+											 int tid,
+											 std::shared_ptr<MyUniqueIdHandler> handler,
+											 NebulaClientPool<MyComposePostServiceClient> *clientPool) {
+
+  // LOG(warning) << "User TID: " << tid << " TID: " << std::this_thread::get_id();
+  RandomGenerator randGen(tid);
+
+  uint64_t buffer_size = NUM_MSGS_PER_CLIENT * BASE_BUFFER_SIZE;
+  
+  MyThriftClient<MyUniqueIdServiceClient>* clients[NUM_TEMPLATE_CLIENTS];
+
+	for (int i = 0; i < NUM_TEMPLATE_CLIENTS; i++) {
+		clients[i] = new MyThriftClient<MyUniqueIdServiceClient>(buffer_size);
+		GenRequests(clients[i], &randGen);
+	}
+
+  std::shared_ptr<MyUniqueIdServiceProcessor> proc = 
+		std::make_shared<MyUniqueIdServiceProcessor>(handler);
+
+	auto processor = new NebulaThriftProcessor<MyUniqueIdServiceProcessor, MyUniqueIdServiceClient>(ctx, tid, proc, clients);
+
+	auto f2cMap = clientPool->AddToPool(ctx->getQP(tid));
+	InitializeFunctionMap(f2cMap, &randGen);
+
+  // uint8_t* cltIBufPtr, *cltOBufPtr;
+  // uint32_t ISz, OSz;
+  
+  // clientPtr->GetBuffer(&cltIBufPtr, &ISz, &cltOBufPtr, &OSz);
+
+  // std::cout << "ISz: " <<  ISz << " OSz: " << OSz << std::endl;
+
+  pthread_barrier_wait(&barrier);
+
+  if (tid == 0) {
+		fprintf(stdout,"Init done! Ready to start execution!\n");
+    ctx->readyForTiming();
+
+    start = true;
+    // LOG(warning) << "Process Phase Started!!";
+  }
+
+  while(!start);
+
+  uint64_t count = 1;
+
+  while (count <= num_iterations) {
+    processor->process(count);
+
+    // std::cout << "Processing Thread " << tid << " count=" << count  << std::endl;
+    // ClientRecvUniqueId(uniqueIdClient);
+
+    count++;
+  }
+
+  #ifdef SW
+    processor->printSWResults();
+  #endif
+
+  // uint8_t* cltIBufPtr, *cltOBufPtr;
+  // uint32_t ISz, OSz;
+  
+  // clientPtr->GetBuffer(&cltIBufPtr, &ISz, &cltOBufPtr, &OSz);
+
+  // std::cout << "ISz: " <<  ISz << " OSz: " << OSz << std::endl;
+
+  // if (tid == max_tid)
+  //   LOG(warning) << "Process Phase finished!";
+}
+
+int main(int argc, char *argv[]) {
+  init_logger();
+
+  uint64_t num_threads, num_nodes;
+
+  if (argc != 4) {
+    cout << "Invalid input! Usage: " << argv[0] << " <total # nodes> <num_threads> <iterations> \n" << endl;
+    exit(-1);
+  } else {
+    num_nodes = atoi(argv[1]);
+    num_threads = atoi(argv[2]);
+    num_iterations = atoi(argv[3]);
+  }
+
+  cpu_set_t  mask;
+  CPU_ZERO(&mask);
+  CPU_SET(1, &mask); // pin the main thread to core #1
+  sched_setaffinity(0, sizeof(mask), &mask);
+
+  pthread_barrier_init(&barrier, NULL, num_threads);
+
+  int node_id = 1;
+  // Create the RPC NUMA Context!
+  rpcNUMAContext* rpcContext = new rpcNUMAContext(node_id, num_nodes, num_threads+1);
+
+  if (GetMachineId(&machine_id) != 0) {
+    exit(EXIT_FAILURE);
+  }
+
+	NebulaClientPool<MyComposePostServiceClient> compose_post_pool("compose-post", BUFFER_SIZE, rpcContext);
+  std::shared_ptr<MyUniqueIdHandler> handler = std::make_shared<MyUniqueIdHandler>(
+                                              &thread_lock, machine_id, &compose_post_pool);
+  
+  std::thread processThreads[num_threads+1];
+
+  int coreID = 0;
+  while (coreID <= num_threads) {
+
+    if (coreID == 1) { //skip this core, this will only run the main thread
+      coreID++;
+      continue;
+    }
+
+    processThreads[coreID] = std::thread(GenAndProcessReqs,
+                                          rpcContext,
+                                          coreID,
+                                          handler,
+																					&compose_post_pool);
+
+    CPU_ZERO(&mask);
+    CPU_SET(coreID, &mask);
+    int error = pthread_setaffinity_np(processThreads[coreID].native_handle(), sizeof(cpu_set_t), &mask);
+    if (error) {
+      printf("Could not bind thread %d to core %d! (error %d)\n", coreID, coreID, error);
+    }
+    coreID++;
+  }
+
+  coreID = 0;
+  while (coreID <= num_threads) {
+    if (coreID == 1) { //skip this core, this will only run the main thread
+      coreID++;
+      continue;
+    }
+
+    processThreads[coreID].join();
+
+    coreID++;
+  }
+
+  return 0;
+}
