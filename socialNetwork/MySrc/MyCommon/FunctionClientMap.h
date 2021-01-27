7f12b4e620a2595128e6da06f8a1be26d919740a
Arash Pourhabibi <arash.pourhabibi@epfl.ch>
2021-01-27 20:56:20 +0100

    Remove extra files.

--- socialNetwork/MySrc/MyCommon/FunctionClientMap.h
+++ socialNetwork/MySrc/MyCommon/FunctionClientMap.h
@@ -38,37 +38,4 @@ MyThriftClient<TThriftClient>* FunctionClientMap<TThriftClient>::GetClient(int f
 	return _client_map[fid][cid];
 }
 
-
-// Helper functions
-
-// #include "../gen-cpp/FakeRedis.h"
-// #include "../gen-cpp/FakeRabbitmq.h"
-// #include "../gen-cpp/PostStorageService.h"
-// #include "../gen-cpp/UserTimelineService.h"
-
-// #include <RandomGenerator.h>
-
-// void InitializeFuncMapRedis(FunctionClientMap<FakeRedisClient> *f2cmap,
-// 																RandomGenerator *randGen,
-// 																int num_template_clients,
-// 																int num_msg_per_client,
-// 																int base_buffer_size);
-
-// void InitializeFuncMapPostStorage(FunctionClientMap<PostStorageServiceClient> *f2cmap,
-// 																RandomGenerator *randGen,
-// 																int num_template_clients,
-// 																int num_msg_per_client,
-// 																int base_buffer_size);																
-
-// void InitializeFuncMapUserTimeline(FunctionClientMap<UserTimelineServiceClient> *f2cmap,
-// 																RandomGenerator *randGen,
-// 																int num_template_clients,
-// 																int num_msg_per_client,
-// 																int base_buffer_size);																
-
-// void InitializeFuncMapRabbitmq(FunctionClientMap<FakeRabbitmqClient> *f2cmap,
-// 																RandomGenerator *randGen,
-// 																int num_template_clients,
-// 																int num_msg_per_client,
-// 																int base_buffer_size);																
 #endif //FUNCTIONCLIENTMAP_H
